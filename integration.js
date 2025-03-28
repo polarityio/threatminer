ea39b76c329cd8a451116b00c28ba3b0228456a8
Ed <ed@polarity.io>
2025-03-28 06:46:40 -0400

    Improve error handling when service is not available

--- integration.js
+++ integration.js
@@ -55,26 +55,38 @@ function startup(logger) {
 }
 
 function _setupRegexBlocklists(options) {
-  if (options.domainBlocklistRegex !== previousDomainRegexAsString && options.domainBlocklistRegex.length === 0) {
+  if (
+    options.domainBlocklistRegex !== previousDomainRegexAsString &&
+    options.domainBlocklistRegex.length === 0
+  ) {
     Logger.debug('Removing Domain Blocklist Regex Filtering');
     previousDomainRegexAsString = '';
     domainBlocklistRegex = null;
   } else {
     if (options.domainBlocklistRegex !== previousDomainRegexAsString) {
       previousDomainRegexAsString = options.domainBlocklistRegex;
-      Logger.debug({ domainBlocklistRegex: previousDomainRegexAsString }, 'Modifying Domain Blocklist Regex');
+      Logger.debug(
+        { domainBlocklistRegex: previousDomainRegexAsString },
+        'Modifying Domain Blocklist Regex'
+      );
       domainBlocklistRegex = new RegExp(options.domainBlocklistRegex, 'i');
     }
   }
 
-  if (options.ipBlocklistRegex !== previousIpRegexAsString && options.ipBlocklistRegex.length === 0) {
+  if (
+    options.ipBlocklistRegex !== previousIpRegexAsString &&
+    options.ipBlocklistRegex.length === 0
+  ) {
     Logger.debug('Removing IP Blocklist Regex Filtering');
     previousIpRegexAsString = '';
     ipBlocklistRegex = null;
   } else {
     if (options.ipBlocklistRegex !== previousIpRegexAsString) {
       previousIpRegexAsString = options.ipBlocklistRegex;
-      Logger.debug({ ipBlocklistRegex: previousIpRegexAsString }, 'Modifying IP Blocklist Regex');
+      Logger.debug(
+        { ipBlocklistRegex: previousIpRegexAsString },
+        'Modifying IP Blocklist Regex'
+      );
       ipBlocklistRegex = new RegExp(options.ipBlocklistRegex, 'i');
     }
   }
@@ -86,18 +98,25 @@ function _isEntityBlocklisted(entity, { blocklist }) {
   const entityIsBlocklisted = _.includes(blocklist, entity.value.toLowerCase());
 
   const ipIsBlocklisted =
-    entity.isIP && !entity.isPrivateIP && ipBlocklistRegex !== null && ipBlocklistRegex.test(entity.value);
-  if (ipIsBlocklisted) Logger.debug({ ip: entity.value }, 'Blocked BlockListed IP Lookup');
+    entity.isIP &&
+    !entity.isPrivateIP &&
+    ipBlocklistRegex !== null &&
+    ipBlocklistRegex.test(entity.value);
+  if (ipIsBlocklisted)
+    Logger.debug({ ip: entity.value }, 'Blocked BlockListed IP Lookup');
 
   const domainIsBlocklisted =
-    entity.isDomain && domainBlocklistRegex !== null && domainBlocklistRegex.test(entity.value);
-  if (domainIsBlocklisted) Logger.debug({ domain: entity.value }, 'Blocked BlockListed Domain Lookup');
+    entity.isDomain &&
+    domainBlocklistRegex !== null &&
+    domainBlocklistRegex.test(entity.value);
+  if (domainIsBlocklisted)
+    Logger.debug({ domain: entity.value }, 'Blocked BlockListed Domain Lookup');
 
   return entityIsBlocklisted || ipIsBlocklisted || domainIsBlocklisted;
 }
 
 function _isInvalidEntity(entity) {
-  return entity.isIPv4 && IGNORED_IPS.has(entity.value)
+  return entity.isIPv4 && IGNORED_IPS.has(entity.value);
 }
 
 function doLookup(entities, options, cb) {
@@ -116,22 +135,22 @@ function doLookup(entities, options, cb) {
       };
 
       if (entity.isHash) {
-        requestOptions.uri = `${options.host}/v2/sample.php`,
-        requestOptions.qs = {q: `${entity.value}`, rt: '1'}
+        (requestOptions.uri = `${options.host}/v2/sample.php`),
+          (requestOptions.qs = { q: `${entity.value}`, rt: '1' });
       } else if (entity.isDomain) {
-        requestOptions.uri = `${options.host}/v2/domain.php`,
-        requestOptions.qs = {q: `${entity.value}`, rt: '1'}
+        (requestOptions.uri = `${options.host}/v2/domain.php`),
+          (requestOptions.qs = { q: `${entity.value}`, rt: '1' });
       } else if (entity.isIPv4) {
-        requestOptions.uri = `${options.host}/v2/host.php`,
-        requestOptions.qs = {q: `${entity.value}`, rt: '1'}
+        (requestOptions.uri = `${options.host}/v2/host.php`),
+          (requestOptions.qs = { q: `${entity.value}`, rt: '1' });
       } else {
         return;
       }
 
       Logger.trace({ uri: requestOptions }, 'Request URI');
 
-      tasks.push(function(done) {
-        requestWithDefaults(requestOptions, function(error, res, body) {
+      tasks.push(function (done) {
+        requestWithDefaults(requestOptions, function (error, res, body) {
           let processedResult = handleRestError(error, entity, res, body);
 
           if (processedResult.error) {
@@ -153,7 +172,12 @@ function doLookup(entities, options, cb) {
     }
 
     results.forEach((result) => {
-      if (result.body === null || result.body.length === 0 || result.body.results === null || result.body.results.length === 0) {
+      if (
+        result.body === null ||
+        result.body.length === 0 ||
+        result.body.results === null ||
+        result.body.results.length === 0
+      ) {
         lookupResults.push({
           entity: result.entity,
           data: null
@@ -162,7 +186,7 @@ function doLookup(entities, options, cb) {
         lookupResults.push({
           entity: result.entity,
           data: {
-            summary: [],
+            summary: [`Total Results: ${result.body.results.length}`],
             details: result.body
           }
         });
@@ -176,6 +200,8 @@ function doLookup(entities, options, cb) {
 
 function handleRestError(error, entity, res, body) {
   let result;
+
+  Logger.info({ res, body }, 'Response');
 
   if (error) {
     return {
@@ -204,15 +230,20 @@ function handleRestError(error, entity, res, body) {
     };
   } else {
     // unexpected status code
+    // We've seen isntances where the ThreatMiner server returns no `body` and only an HTTP statuscode
+    // so we added guards for an undefined `body` parameter.
     result = {
-      error: body,
-      detail: `${body.error}: ${body.message}`
+      error: body ? body : 'Unknown Error',
+      detail:
+        body && body.error && body.message
+          ? `${body.error}: ${body.message}`
+          : `Unexpected HTTP Status Code ${res.statusCode} received.`
     };
   }
   return result;
 }
 
 module.exports = {
-  doLookup: doLookup,
-  startup: startup
+  doLookup,
+  startup
 };
